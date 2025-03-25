0546e243b1983fd7db4a8e273be30fc25e811184
YasinSeyhun <seyhunyasin@gmail.com>
2025-03-25 17:18:28 +0300

    new methods

---
+++ MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return RedirectToAction("Index","Ornek");
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("ornekcontentsayfasi");
+        }
+
+        public IActionResult JsonResult()
+        {
+            return Json(new { name = "value" });
+        }
+
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
+
+        public IActionResult ParametreView(int id)
+        {
+            return RedirectToAction("JsonResultParametre", "Ornek", new { id = id });
+        }
+
+        public IActionResult JsonResultParametre(int id)
+        {
+            return Json(new { id = id });
+        }
+    }
+}
